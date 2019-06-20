fafe58a3892d53c15015186bb21f5dc653bd1d21
duy.ton <duy.ton@exe.com.vn>
2019-06-20 17:17:08 +0700

    recreate a fully source of dotnet core

---
+++ DNC.Reminder/Reminder.Cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Reminder.Cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ DNC.Reminder/Reminder.Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Reminder.Common
+{
+    public class Class1
+    {
+    }
+}
